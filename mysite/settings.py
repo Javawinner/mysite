4f09cfa46bbbdf2cc497cfed8d523c8da19ee835
Java_winner <39736065+Javawinner@users.noreply.github.com>
2019-10-22 22:40:51 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',  # 连接数据库的类型
+        'NAME': 'mysite',  # 数据库名
+        'HOST': '127.0.0.1',  # 数据库主机地址
+        'PORT': 3306,  # 数据库的端口
+        'USER': 'root',
+        'PASSWORD': '123',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
