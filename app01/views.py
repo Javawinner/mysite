ac90cb4ec507a332d9752d551508a9e2b2718901
Java_winner <15618518792@163.com>
2019-10-22 21:20:26 +0800

    加入了index

--- app01/views.py
+++ app01/views.py
@@ -4,7 +4,21 @@ from django.shortcuts import render, HttpResponse, redirect
 
 
 def login(request):
-    return render(request, 'login.html')
+    error_msg = ''
+    if request.method == 'POST':
+        email, pwd = request.POST.get('email'), request.POST.get('pwd')
+        print(email, pwd)
+
+        if email == 'lyysb@123.com' and pwd == '123':
+            return redirect('/index/')
+        else:
+            error_msg = '邮箱或密码错误'
+
+    return render(request, 'login.html', {'error_msg': error_msg})
+
+def index(request):
+    return render(request, 'index.html')
+
 
 
 
